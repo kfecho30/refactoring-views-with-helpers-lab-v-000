8e7a3d74ea64046a4a8717e5dba33e12262d7865
Kelsey Fecho <kfecho30@gmail.com>
2018-05-07 19:22:23 +0000

    automatically backed up by learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -4,7 +4,7 @@ module ArtistsHelper
     if song.artist.present?
       link_to song.artist_name, artist_path(song.artist)
     else
-      #link "Add Artist" to song's edit page
+      link_to "Add Artist", edit_song_path(song)
     end
   end
 end
