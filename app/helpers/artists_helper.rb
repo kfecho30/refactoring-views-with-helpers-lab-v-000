32bc2b5cf11eef1701de5bc399fbe016f97dd072
Kelsey Fecho <kfecho30@gmail.com>
2018-05-07 19:20:28 +0000

    automatically backed up by learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -2,7 +2,7 @@ module ArtistsHelper
 
   def display_artist(song)
     if song.artist.present?
-      <a href="artists/#{song.artist.id}"></a>#return link to artist show page
+      #return link to artist show page
     else
       #link "Add Artist" to song's edit page
     end
